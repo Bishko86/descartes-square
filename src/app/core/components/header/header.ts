c4d6b91d92b12cbbe1a1964669989068695b5347
roman.bishko@loopme.com <roman.bishko@loopme.com>
2025-07-12 00:38:03 +0300

    add menu

--- src/app/core/components/header/header.ts
+++ src/app/core/components/header/header.ts
@@ -1,11 +1,16 @@
 import {Component} from '@angular/core';
+import {MENU_ITEMS} from '@core/consts/menu-items.const';
+import {RouterLink} from '@angular/router';
+import {MenuItem} from '@core/interfaces/menu-item.interface';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
-
+  public menuItems: MenuItem[] = MENU_ITEMS;
 }
