e98d3b7e1701afab0671775c49d162bcf869565f
roman.bishko@loopme.com <roman.bishko@loopme.com>
2025-07-12 23:41:37 +0300

    update app routes configuration with menu paths and lazy-loaded components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {MenuRoutes} from '@core/enums/menu-routes.enum';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path: '', redirectTo: 'home', pathMatch: 'full'}, {
+  path: MenuRoutes.DESCARTES_SQUARE,
+  loadComponent: () => import('./descartes-square/descartes-square/descartes-square').then((mod) => mod.DescartesSquare)
+}, {
+  path: MenuRoutes.HOME,
+  loadComponent: () => import('./home/home/home').then((mod) => mod.Home)
+}];
