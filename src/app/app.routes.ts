f257bae69d58e50ae510e400ad36858ab0e0359c
roman.bishko@loopme.com <roman.bishko@loopme.com>
2025-07-21 00:21:26 +0300

    add Descartes List component with template, styles, and setup; update routes to include "list" child route for Descartes Square

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,29 @@
 import {Routes} from '@angular/router';
 import {MenuRoutes} from '@core/enums/menu-routes.enum';
 
-export const routes: Routes = [{path: '', redirectTo: 'home', pathMatch: 'full'}, {
-  path: MenuRoutes.DESCARTES_SQUARE,
-  loadComponent: () => import('./descartes-square/descartes-square/descartes-square').then((mod) => mod.DescartesSquare),
-  children: [{
-    path: 'create',
-    loadComponent: () => import('./descartes-square/descartes-form/descartes-form').then((mod) => mod.DescartesForm)
-  },]
-}, {
-  path: MenuRoutes.HOME,
-  loadComponent: () => import('./home/home/home').then((mod) => mod.Home)
-}];
+export const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: MenuRoutes.DESCARTES_SQUARE,
+    loadComponent: () => import('./descartes-square/descartes-square/descartes-square').then((mod) => mod.DescartesSquare),
+    children: [
+      {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full'
+      },
+      {
+        path: 'create',
+        loadComponent: () => import('./descartes-square/descartes-form/descartes-form').then((mod) => mod.DescartesForm)
+      },
+      {
+        path: 'list',
+        loadComponent: () => import('./descartes-square/descartes-list/descartes-list').then((mod) => mod.DescartesList)
+      },
+    ]
+  }, {
+    path: MenuRoutes.HOME,
+    loadComponent: () => import('./home/home/home').then((mod) => mod.Home)
+  }];
---
+++ src/app/descartes-square/descartes-list/descartes-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-descartes-list',
+  imports: [],
+  templateUrl: './descartes-list.html',
+  styleUrl: './descartes-list.scss'
+})
+export class DescartesList {
+
+}
