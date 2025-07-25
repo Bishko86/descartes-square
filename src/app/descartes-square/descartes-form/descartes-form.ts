5bd55b6bba223a5ab35da3f18767731b39f32335
roman.bishko@loopme.com <roman.bishko@loopme.com>
2025-07-25 09:33:06 +0300

    update Descartes List: implement table with Angular Material, add CRUD functionality with localStorage integration, and enhance template with MoreOptions component

--- src/app/descartes-square/descartes-form/descartes-form.ts
+++ src/app/descartes-square/descartes-form/descartes-form.ts
@@ -4,8 +4,11 @@ import {IDescartesForm, TFormNames} from '../definitions/interfaces/descartes-fo
 import {NgTemplateOutlet} from '@angular/common';
 import {Maybe} from '@core/types/maybe.type';
 import {LocalStorageKeys} from '@core/enums/local-storage-key.enum';
-import {MatSnackBar, MatSnackBarModule, MatSnackBarRef} from '@angular/material/snack-bar';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 import {IDescartesSolution} from '../definitions/interfaces/descartes-solution.interface';
+import {MatButton} from '@angular/material/button';
+import {Router} from '@angular/router';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-descartes-form',
@@ -13,20 +16,26 @@ import {IDescartesSolution} from '../definitions/interfaces/descartes-solution.i
     ReactiveFormsModule,
     NgTemplateOutlet,
     MatSnackBarModule,
+    MatButton,
+    MatTooltipModule,
   ],
   templateUrl: './descartes-form.html',
   styleUrl: './descartes-form.scss'
 })
 export class DescartesForm implements OnInit {
-  id = input<string>();
-  form: FormGroup<IDescartesForm>;
-  formEditTracker = new Map<TFormNames, boolean>()
-    .set('q1', false)
-    .set('q2', false)
-    .set('q3', false)
-    .set('q4', false);
+  readonly id = input<string>();
 
-  #snackBar = inject(MatSnackBar);
+  form: FormGroup<IDescartesForm>;
+
+  formEditTracker = new Map<TFormNames, Maybe<number>>()
+    .set('q1', null)
+    .set('q2', null)
+    .set('q3', null)
+    .set('q4', null);
+
+  readonly #snackBar = inject(MatSnackBar);
+
+  readonly #router = inject(Router);
 
   ngOnInit(): void {
     this.#initForm();
@@ -34,9 +43,10 @@ export class DescartesForm implements OnInit {
 
   addArgument(key: TFormNames): void {
     const formArray = this.#getArgumentForm(key);
+
     if (formArray.valid) {
       formArray.push(new FormControl('', Validators.required));
-      this.formEditTracker.set(key, true);
+      this.formEditTracker.set(key, formArray.length - 1);
     }
   }
 
@@ -44,16 +54,25 @@ export class DescartesForm implements OnInit {
     const formArray = this.#getArgumentForm(key);
 
     if (index === formArray.length - 1) {
-      this.formEditTracker.set(key, false);
+      this.formEditTracker.set(key, null);
     }
 
     formArray.removeAt(index);
   }
 
+  editArgument(index: number, key: TFormNames,): void {
+    const formArray = this.#getArgumentForm(key);
+    const control = formArray.at(index);
+
+    if (formArray.valid) {
+      this.formEditTracker.set(key, index);
+    }
+  }
+
   saveArgument(key: TFormNames): void {
     const formArray = this.#getArgumentForm(key);
     if (formArray.valid) {
-      this.formEditTracker.set(key, false);
+      this.formEditTracker.set(key, null);
     }
   }
 
@@ -76,6 +95,10 @@ export class DescartesForm implements OnInit {
     this.#snackBar.open('Form saved', 'Close', {})
     this.form.markAsPristine();
     this.form.markAsUntouched();
+  }
+
+  cancelForm(): void {
+    this.#router.navigate(['descartes-square']).then();
   }
 
   #getArgumentForm(key: string): FormArray<FormControl<Maybe<string>>> {
