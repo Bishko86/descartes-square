6164eccbf38bf8fcebc810927d702a1136c30066
roman.bishko@loopme.com <roman.bishko@loopme.com>
2025-07-29 23:01:14 +0300

    add Confirm component and service: implement reusable confirmation dialog with Angular Material, styles, and logic

---
+++ src/app/core/components/confirm/confirm.ts
@@ -0,0 +1,25 @@
+import {Component, inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {MatButton} from '@angular/material/button';
+
+@Component({
+  selector: 'app-confirm',
+  imports: [
+    MatButton
+  ],
+  templateUrl: './confirm.html',
+  styleUrl: './confirm.scss'
+})
+export class Confirm {
+  readonly dialog = inject(MatDialogRef<Confirm>);
+  readonly title: string = inject(MAT_DIALOG_DATA);
+
+
+  confirmed(): void {
+    this.dialog.close(true);
+  }
+
+  cancelled(): void {
+    this.dialog.close(false);
+  }
+}
