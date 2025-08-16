c718cae8c425dbf31ed51eeefbaa478c89e00cca
roman.bishko <roman.bishko@loopme.com>
2025-08-16 22:22:44 +0300

    enable CORS in API, add environment configurations and file replacements in Descartes Square

---
+++ apps/descartes-square/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.descartes-square.com/api',
+  appName: 'Descartes Square',
+  version: '1.0.0',
+  enableDebugTools: false,
+  logLevel: 'error',
+};
