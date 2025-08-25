d5d5af239adb6b8d7734027840e40ab4ce1b79cd
roman.bishko <roman.bishko@loopme.com>
2025-08-25 15:33:51 +0300

    add sign-in/up components

---
+++ libs/shared-ui/src/lib/sign-in/sign-in.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-sign-in',
+  imports: [CommonModule],
+  templateUrl: './sign-in.html',
+  styleUrl: './sign-in.scss',
+})
+export class SignIn {}
