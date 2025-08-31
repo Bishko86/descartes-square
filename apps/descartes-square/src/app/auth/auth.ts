6ed84e50cd46a559561335e88729bb4c2d1e2a42
roman.bishko <roman.bishko@loopme.com>
2025-08-31 14:17:12 +0300

    fix build

--- apps/descartes-square/src/app/auth/auth.ts
+++ apps/descartes-square/src/app/auth/auth.ts
@@ -5,6 +5,8 @@ import { AuthService } from '@shared-ui/src/lib/auth/services/auth.service';
 import { DescartesAuthService } from './services/descartes-auth.service';
 import { Router } from '@angular/router';
 import { MenuRoutes } from '@core/enums/menu-routes.enum';
+import { Maybe } from '@shared/src/lib/types/maybe.type';
+import { iif, of, tap } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -14,7 +16,19 @@ import { MenuRoutes } from '@core/enums/menu-routes.enum';
 })
 export class Auth {
   #router = inject(Router);
-  submit(): void {
-    this.#router.navigate([MenuRoutes.DESCARTES_SQUARE]).then();
+  #authService = inject(DescartesAuthService);
+
+  submit(userId: Maybe<string>): void {
+    iif(
+      () => !!userId,
+      this.#authService.getUserById(userId as string),
+      of(null),
+    )
+      .pipe(
+        tap(() => {
+          this.#router.navigate([MenuRoutes.DESCARTES_SQUARE]).then();
+        }),
+      )
+      .subscribe();
   }
 }
