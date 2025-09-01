0e7f4fe5b51c45b9c5d02d74f199cb42614a8a82
roman.bishko <roman.bishko@loopme.com>
2025-09-02 00:12:56 +0300

    refactoring; move descartes components to separate directory

--- apps/descartes-square/src/app/descartes-square/definitions/interfaces/descartes-solution.interface.ts
+++ apps/descartes-square/src/app/descartes-square/definitions/interfaces/descartes-solution.interface.ts
@@ -1,9 +1,9 @@
 import { Maybe } from '@shared/src/lib/types/maybe.type';
 
 export interface IDescartesSolution {
-  id: string;
-  title: string;
-  conclusion: string;
+  id?: string;
+  title: Maybe<string>;
+  conclusion: Maybe<string>;
   q1: Maybe<string[]>;
   q2: Maybe<string[]>;
   q3: Maybe<string[]>;
