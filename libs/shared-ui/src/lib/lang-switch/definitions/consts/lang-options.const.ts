51d1588aeee84b932112c58577738b52b858498d
roman.bishko <roman.bishko@loopme.com>
2026-02-27 13:55:12 +0200

    refactor(lang-switch): enhance locale handling with dynamic path detection; rename `UA` to `UK` in enums and constants

--- libs/shared-ui/src/lib/lang-switch/definitions/consts/lang-options.const.ts
+++ libs/shared-ui/src/lib/lang-switch/definitions/consts/lang-options.const.ts
@@ -3,4 +3,4 @@ import { ILangConfig } from '../interfaces/lang-config.interface';
 
 export const LangOptionsMap = new Map<LangCode, ILangConfig>()
   .set(LangCode.EN, { code: LangCode.EN, name: 'English', flag: '🇬🇧' })
-  .set(LangCode.UA, { code: LangCode.UA, name: 'Українська', flag: '🇺🇦' });
+  .set(LangCode.UK, { code: LangCode.UK, name: 'Українська', flag: '🇺🇦' });
